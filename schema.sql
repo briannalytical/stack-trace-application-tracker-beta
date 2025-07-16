e8af8d39ea4446b3f2aeaa69b554e3900bdc961f
bidgeycodes <brianna.c.mcneil@gmail.com>
2025-07-16 11:48:38 -0400

    updates to schema

--- schema.sql
+++ schema.sql
@@ -2,9 +2,13 @@ CREATE TABLE job_applications (
     id SERIAL PRIMARY KEY,
     job_title TEXT NOT NULL,
     company TEXT NOT NULL,
-    date_applied SET DEFAULT CURRENT_DATE,
+    date_applied TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    application_software TEXT NOT NULL,
     application_status TEXT DEFAULT 'applied',
-	next_action TEXT DEFAULT 'check application status',
-    check_application_status DATE,
-    application_software TEXT NOT NULL
+    next_action TEXT DEFAULT 'check application status',
+    check_application_status TIMESTAMP,
+    checked_application BOOLEAN NOT NULL,
+    follow_up_contact_name TEXT,
+    follow_up_contact_details TEXT,
+    notes TEXT
 );
